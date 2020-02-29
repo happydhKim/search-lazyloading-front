a58d0a221c56484cdcf768608a0809b656d241e6
happydhKim <happytime870616@gmail.com>
2020-02-29 22:29:56 +0900

    [ FEATURE ] styled 세팅 및 styled.d 작업 (#2)
    Authored by: happydhKim <happydhKim870@gmail.com>
    
    Why
    - styled를 세팅하여, App.tsx에 연동 해야한다
    - styled.d를 설계해서 추가적인 작업을 가능하게 한다
    
    What
    - styled 설치 및 styled를 이용해 전역 변수를 사용 할 수 있도록 설계
    - styled 를 이용해 theme을 사용할 수 있도록 ThemeProvider 설계 및 적용
    - 이전 작업에서 밀린 webpack eslint 에러 문제를 output을 추가 해줘서 수정
    - styled.d 파일을 추가하여 확장성을 높일 수 있도록 설계
    
    Reference
    - https://styled-components.com/docs/api#typescript
    - https://www.typescriptlang.org/docs/handbook/tsconfig-json.html

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle, theme } from './styles';
 
-const App = () => (
-  <>
+const App: React.FC = () => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
     <div>
       Hello React
     </div>
-  </>
+  </ThemeProvider>
 );
 
 export default App;
---
+++ web/src/styles/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  colors: {
+    blue: '#2179ee',
+    green: '#00cc9a',
+    coral: '#ff6759',
+    gold: '#f0b95b',
+    purple: '#7537ef',
+    white: '#ffffff',
+    black: '#000000',
+  },
+};
+
+export default theme;
