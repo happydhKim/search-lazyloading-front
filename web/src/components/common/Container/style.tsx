954293ff56ce88da6c859dd7455f036f80417341
happydhKim <happytime870616@gmail.com>
2020-03-01 23:28:23 +0900

    [ FEATURE ] 반응형 웹 설계 및 테스트 (#3)
    Authored by: happydhKim <happydhKim870@gmail.com>
    
    Why
    - 반응형 웹을 사용할 방법을 찾고 테스트 해봐야 한다
    
    What
    - 반응형 웹을 구현하기 위한 방법을 찾고 @media를 사용한 방식을 채택함
    - @media 방식을 Header 컴포넌트를 통해 테스트 해본 결과 정상적으로 작동함
    - 다음 작업을 진행하기 위해 괜찮은 서비스 웹을 찾아 디자인을 클론 계획을 해야 함
    
    Reference
    - https://developers.google.com/web/fundamentals/design-and-ux/responsive/?hl=ko
    - https://googlesamples.github.io/web-fundamentals/fundamentals/design-and-ux/responsive/weather-2.html

---
+++ web/src/components/common/Container/style.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const S = {
+  ContainerWrapper: styled.div`
+    margin-top: 66px;
+    height: 100%;
+    background-color: powderblue;
+  `,
+};
+
+export default S;
--- web/src/components/common/Header/Header.tsx
+++ web/src/components/common/Header/Header.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
+import S from './style';
 
 const Header: React.FC = () => {
   const a = 3;
 
   return (
-    <>
+    <S.HeaderWrapper>
       Header
-    </>
+    </S.HeaderWrapper>
   );
 };
 
